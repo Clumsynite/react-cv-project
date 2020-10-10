1f38252bee289062796a03e852a15851aa435dbe
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 17:00:12 +0530

    updated Personal template to adjust with new props

--- src/components/Templates/Personal.js
+++ src/components/Templates/Personal.js
@@ -5,14 +5,14 @@ export default class Personal extends Component {
     super();
   }
   render() {
-    const { name, description, email } = this.props;
+    const { details } = this.props;
     return (
       <div className="Personal">
         <div className="card">
           <div className="card-body">
-            <h5 className="card-title">{name}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{email}</h6>
-            <p className="card-text">{description}</p>
+            <h5 className="card-title">{`${details.firstname} ${details.lastname}`}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{details.email}</h6>
+            <p className="card-text">{details.address}</p>
           </div>
         </div>
       </div>
