1c4b302c01b428e84c6fc755a036d5d3623ff057
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 16:59:36 +0530

    added language component
    to add known programming languages

---
+++ src/components/Templates/Languages.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Language extends Component {
+  constructor() {
+    super()
+  }
+  render(){
+    return (
+      <div className="Language">
+        
+      </div>
+    )
+  }
+}
