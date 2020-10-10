e1d32652828419d12d2e0ac69623dd30119d943f
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 17:58:32 +0530

    Improved card for Personal Section

--- src/components/Templates/Personal.js
+++ src/components/Templates/Personal.js
@@ -5,14 +5,18 @@ export default class Personal extends Component {
     super();
   }
   render() {
-    const { details } = this.props;
+    const { firstname, lastname, email, address, edit } = this.props;
     return (
       <div className="Personal">
         <div className="card">
+        <div className="card-header d-flex justify-content-between align-items-center">
+          <h5>Personal Details</h5>
+          <button className="btn btn-outline-primary btn-sm" onClick={edit} title="Edit Section"><i className="material-icons" style={{fontSize: "20px"}}>edit</i></button>
+        </div>
           <div className="card-body">
-            <h5 className="card-title">{`${details.firstname} ${details.lastname}`}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{details.email}</h6>
-            <p className="card-text">{details.address}</p>
+            <h5 className="card-title">{`${firstname} ${lastname}`}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{email}</h6>
+            <p className="card-text">{address}</p>
           </div>
         </div>
       </div>
