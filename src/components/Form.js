b8028b87494a59ab0bfbdc2506fbd6f70c867e1e
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 15:33:19 +0530

    added Forms component to get User details for CV

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class Form extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <div className="Form"></div>;
+  }
+}
