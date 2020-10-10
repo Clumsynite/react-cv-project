4a627a0d4370b2fd734f4ebd860c190ff7032d37
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 16:58:23 +0530

    updated form component
    it'll store data in state
    then validate it
    Every section has a different form and template
    If validated The Form will hide and Template will be displayed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,53 @@
 import React, { Component } from "react";
-
+import PersonalForm from "./Forms/Personal";
+import PersonalTemplate from "./Templates/Personal";
 export default class Form extends Component {
   constructor() {
     super();
+    this.state = {
+      firstname: "",
+      lastname: "",
+      email: "",
+      description: "",
+      address: "",
+      Languages: [],
+      projects: [],
+      education: [],
+      certificates: [],
+      experience: [],
+      renderPersonal: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.savePersonal = this.savePersonal.bind(this);
+  }
+
+  handleChange(e) {
+    const value = e.target.value;
+    this.setState({ [e.target.id]: value });
+  }
+  savePersonal(e) {
+    e.preventDefault();
   }
   render() {
-    return <div className="Form"></div>;
+    const {
+      firstname,
+      lastname,
+      email,
+      description,
+      address,
+      Languages,
+      projects,
+      education,
+      certificates,
+      experience,
+      renderPersonal,
+    } = this.state;
+    return (
+      <div className="Form">
+        <PersonalForm handleChange={this.handleChange} />
+        {renderPersonal && <PersonalTemplate details={firstname, lastname,email, address}/>}
+      </div>
+    );
   }
 }
