e869a346981513144007fbfd1b8af3d4d78df4e5
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 14:28:42 +0530

    added personal component

---
+++ src/componenets/Personal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Personal extends Component {
+  constructor() {
+    super()
+  }
+  render(){
+    return (
+      <div className="Personal">
+        
+      </div>
+    )
+  }
+}
