7fa0908a7045eb0df66a0ee7a59d87e9c475ed0f
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 21:20:04 +0530

    Building experience form

--- src/components/Forms/Experience.js
+++ src/components/Forms/Experience.js
@@ -5,7 +5,17 @@ export default class Experience extends Component {
     return (
       <div className="Experience">
         <div className="card">
-          <div className="card-body bg-light"></div>
+          <div className="card-body bg-light">
+          <div class="form-group">
+              <label for="companyName">Compnay Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="companyName"
+                placeholder="Some Company"
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
