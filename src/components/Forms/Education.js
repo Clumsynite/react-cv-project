f3d6fe96fbbf3a49cd866daf6cd3a32a1ddd9c2f
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 18:50:53 +0530

    Added  Education Form
    Needs Fields

---
+++ src/components/Forms/Education.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Education extends Component {
+  render() {
+    return(
+      <div className="Education">
+        <div className="card">
+          <div className="card-body bg-aqua">
+            
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
