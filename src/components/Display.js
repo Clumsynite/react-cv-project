2c238c1696711d7358d25e713ae9876e14d92f15
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 15:31:26 +0530

    added display component to display components

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Displaye extends Component {
+  constructor() {
+    super()
+  }
+  render(){
+    return (
+      <div className="Displaye">
+        
+      </div>
+    )
+  }
+}
