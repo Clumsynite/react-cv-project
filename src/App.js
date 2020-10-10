1082446ce8d4a896c9fd29d288278425130ed824
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 16:57:08 +0530

    added margin for content

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Form from "./components/Form";
 function App() {
   return (
-    <div className="App">
+    <div className="App container mt-5">
       <Form />
     </div>
   );
