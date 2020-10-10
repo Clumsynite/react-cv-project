602c82b207b4baf2b75463e9fcfbea6f5d831a92
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 21:29:02 +0530

    Updated Experience Form

--- src/components/Forms/Experience.js
+++ src/components/Forms/Experience.js
@@ -6,14 +6,54 @@ export default class Experience extends Component {
       <div className="Experience">
         <div className="card">
           <div className="card-body bg-light">
-          <div class="form-group">
-              <label for="companyName">Compnay Name</label>
+            <div class="form-group">
+              <label for="name">Company Name</label>
               <input
                 type="text"
                 class="form-control"
-                id="companyName"
+                id="name"
                 placeholder="Some Company"
               />
+            </div>
+            <div class="form-group">
+              <label for="position">Job Title</label>
+              <input
+                type="text"
+                class="form-control"
+                id="position"
+                placeholder="Junior/Senior Developer"
+                title="Position"
+              />
+            </div>
+            <div class="form-group">
+              <label for="tasks">Your Tasks</label>
+              <input
+                type="text"
+                class="form-control"
+                id="tasks"
+                placeholder="Describe what you did"
+                title="What did you do over there"
+              />
+            </div>
+            <div className="form-row">
+              <div class="form-group col-md-6">
+                <label for="start">Start Date</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="start"
+                  title="Date when you Started this course"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="end">End Date</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="end"
+                  title="Date when you Finished this course"
+                />
+              </div>
             </div>
           </div>
         </div>
