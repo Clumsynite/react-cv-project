9001270f68a889d7b1858ab8a965f0b132edb740
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 21:19:02 +0530

    adding form for education
    Need to think of a way to keep create multiple entries for education

--- src/components/Forms/Education.js
+++ src/components/Forms/Education.js
@@ -5,7 +5,46 @@ export default class Education extends Component {
     return (
       <div className="Education">
         <div className="card">
-          <div className="card-body bg-aqua"></div>
+          <div className="card-body bg-light">
+            <div class="form-group">
+              <label for="name">School Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="name"
+                placeholder="Tilak Maharashtra Vidyapeeth"
+              />
+            </div>
+            <div class="form-group">
+              <label for="degree">Title of Study</label>
+              <input
+                type="text"
+                class="form-control"
+                id="degree"
+                placeholder="Certification/Degree for? Ex. BCA"
+              />
+            </div>
+            <div className="form-row">
+              <div class="form-group col-md-6">
+                <label for="start">Start Date</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="start"
+                  title="Date when you Started this course"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="end">End Date</label>
+                <input
+                  type="date"
+                  class="form-control"
+                  id="end"
+                  title="Date when you Finished this course"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
