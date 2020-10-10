723a1a9779d5082ac3f7c81628c859e2521e8017
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-10 20:41:35 +0530

    added experience form

---
+++ src/components/Forms/Experience.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Experience extends Component {
+  render() {
+    return (
+      <div className="Experience">
+        <div className="card">
+          <div className="card-body bg-light"></div>
+        </div>
+      </div>
+    );
+  }
+}
