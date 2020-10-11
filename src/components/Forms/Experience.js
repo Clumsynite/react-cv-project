7c46f234e25b60e8b2fadde54437643b283d0237
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-11 14:27:26 +0530

    Added event handlers

--- src/components/Forms/Experience.js
+++ src/components/Forms/Experience.js
@@ -1,60 +1,91 @@
 import React, { Component } from "react";
 
 export default class Experience extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      title: "",
+      start: "",
+      end: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
   render() {
+    const { handleClick } = this.props;
     return (
       <div className="Experience">
         <div className="card">
           <div className="card-body bg-light">
-            <div class="form-group">
-              <label for="name">Company Name</label>
+            <div className="form-group">
+              <label htmlFor="companyName">Company Name</label>
               <input
                 type="text"
-                class="form-control"
-                id="name"
+                className="form-control"
+                id="companyName"
+                name="name"
                 placeholder="Some Company"
+                onChange={this.handleChange}
               />
             </div>
-            <div class="form-group">
-              <label for="position">Job Title</label>
+            <div className="form-group">
+              <label htmlFor="position">Job Title</label>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="position"
+                name="position"
                 placeholder="Junior/Senior Developer"
                 title="Position"
+                onChange={this.handleChange}
               />
             </div>
-            <div class="form-group">
-              <label for="tasks">Your Tasks</label>
+            <div className="form-group">
+              <label htmlFor="tasks">Your Tasks</label>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="tasks"
+                name="tasks"
                 placeholder="Describe what you did"
                 title="What did you do over there"
+                onChange={this.handleChange}
               />
             </div>
             <div className="form-row">
-              <div class="form-group col-md-6">
-                <label for="start">Start Date</label>
+              <div className="form-group col-md-6">
+                <label htmlFor="start">Start Date</label>
                 <input
                   type="date"
-                  class="form-control"
+                  className="form-control"
                   id="start"
+                  name="start"
                   title="Date when you Started this course"
+                  onChange={this.handleChange}
                 />
               </div>
-              <div class="form-group col-md-6">
-                <label for="end">End Date</label>
+              <div className="form-group col-md-6">
+                <label htmlFor="end">End Date</label>
                 <input
                   type="date"
-                  class="form-control"
+                  className="form-control"
                   id="end"
+                  name="end"
                   title="Date when you Finished this course"
+                  onChange={this.handleChange}
                 />
               </div>
             </div>
+            <button
+              type="submit"
+              className="btn btn-outline-primary btn-block"
+              onClick={handleClick}
+            >
+              Add
+            </button>
           </div>
         </div>
       </div>
