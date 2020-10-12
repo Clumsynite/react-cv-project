cd4992492d6e3169a386af0e5e30baca7227faee
Clumsy Knight <65499068+Clumsynite@users.noreply.github.com>
2020-10-12 20:01:49 +0530

    Updates

---
+++ precache-manifest.9ac28ca66cb1bb4598aabb0b8a004ef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a11b92b825f20fb0eeb8491738e8667",
+    "url": "/react-cv-project/index.html"
+  },
+  {
+    "revision": "eea99311ccad2f087ac7",
+    "url": "/react-cv-project/static/js/2.a974b065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cv-project/static/js/2.a974b065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2960305392517fb1542",
+    "url": "/react-cv-project/static/js/main.f21ff59e.chunk.js"
+  },
+  {
+    "revision": "ed8b9f55fed0e0b49e0a",
+    "url": "/react-cv-project/static/js/runtime-main.f59069ad.js"
+  }
+]);
